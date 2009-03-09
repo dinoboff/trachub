7c2523481a8b0ccc74525f304ebee726df73220a
Damien Lebrun <dinoboff@hotmail.com>
2009-03-09 14:48:50 +0000

    add data validation.
    It is incomplete, but I started to validate the data sent by GitHub.
    It validate urls and emails, filter anything else than ascii characters in other fields.

--- src/trac_hub/post_parser.py
+++ src/trac_hub/post_parser.py
@@ -3,10 +3,52 @@ Created on 8 Mar 2009
 
 @author: damien lebrun
 """
+import re
 
 from trac.core import Interface, Component, ExtensionPoint, implements
 from trac.web.api import IRequestHandler
 import simplejson
+
+re_url = re.compile(r"""^
+    (# Scheme
+     (https?|git):
+     (# Authority & path
+      //
+      ([a-z0-9\-._~%!$&'()*+,;=]+@)?              # User
+      ([a-z0-9\-._~%]+                            # Named host
+      |\[[a-f0-9:.]+\]                            # IPv6 host
+      |\[v[a-f0-9][a-z0-9\-._~%!$&'()*+,;=:]+\])  # IPvFuture host
+      (:[0-9]+)?                                  # Port
+      (/[a-z0-9\-._~%!$&'()*+,;=:@]+)*/?          # Path
+     )
+    )
+    # Query
+    (\?[a-z0-9\-._~%!$&'()*+,;=:@/?]*)?
+    # Fragment
+    (\#[a-z0-9\-._~%!$&'()*+,;=:@/?]*)?
+    $""", re.VERBOSE | re.IGNORECASE)
+
+re_email = re.compile(
+    r"""
+    [a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*
+    @(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"""
+    , re.VERBOSE | re.IGNORECASE)
+
+re_general = re.compile(
+    r"""[^-_\!"£\$%\^&\*\(\)\+=\{\}\[\]:;'@~#\|\\,./<>\?a-z0-9\s ]+""",
+    re.IGNORECASE)
+
+def validate_url(url):
+    return bool(re_url.match(url))
+
+def validate_email(email):
+    return bool(re_email.match(email))
+
+def filter(field):
+    return re_general.sub('', field)
+
+class GitHubPostError(Exception):
+    pass
 
 
 class IGitHubPostObservers(Interface):
@@ -44,16 +86,50 @@ class GitHubPostParser(Component):
         status = 200
         try:
             data = req.args.get('payload')
-            self.env.log.debug("Payload to parse: %s" % data)
-            json_data = simplejson.loads(data)
+            json_data = self.validate_payload(data)
             for commit_data in json_data.get('commits'):
                 self.env.log.debug("Calling observer(s) for commit: %s" % commit_data)
                 for observer in self.observers:
                     observer.process_commit(json_data, commit_data)
-        except (ValueError, TypeError), e:
+        except (GitHubPostError), e:
             self.env.log.error('Invalid data (%s).' % str(e))
             msg = 'Invalid data.'
             status = 404
         req.send(msg, content_type='text/plain', status=status)
+        
+    def validate_payload(self, json_string):
+        try:
+            self.env.log.debug("Payload to parse: %s" % json_string)
+            json_data = simplejson.loads(json_string)
+        except(ValueError, TypeError), e:
+            raise GitHubPostError(
+                'Wrong json syntax (%s) in: %s' % (str(e), json_string,))
+        
+        self.validate_fields(json_data)
+        
+        return json_data
+    
+    def validate_fields(self, fields):
+        for field in fields:
+            if field in ('owner','author','repository'):
+                self.validate_fields(fields[field])
+            elif field == 'commits':
+                for commit in fields[field]:
+                    self.validate_fields(commit)                    
+            elif field == 'email':
+                if not validate_email(fields[field]):
+                    raise GitHubPostError(
+                        '%s is not valid' % fields[field])
+            elif field == 'url':
+                if not validate_url(fields[field]):
+                    raise GitHubPostError(
+                        '%s is not url' % fields[field])
+            else:
+                try:
+                    fields[field] = filter(fields[field])
+                except:
+                    fields[field] = None
+                    
+        #TODO: validate required fields
 
 
